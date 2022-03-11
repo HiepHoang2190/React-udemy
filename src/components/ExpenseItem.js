8b14e136ff202f9dee9247dee611c2608682a9b7
hieplotus <hieph@lotusoutsourcing.com>
2022-03-11 16:26:49 +0700

    video-37-Passing Data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './ExpenseItem.css';
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   const expenseDate = new Date(2021,2,28);
   const expenseTitle = 'Car Insurance';
   const expenseAmount = 294.67;
-
+  const {title,amount, date} = props;
   return (
    <div className="expense-item">
-     <div>{expenseDate.toDateString()}</div>
+     <div>{date.toDateString()}</div>
      <div className="expense-item__description">
-       <h2>{expenseTitle}</h2>
-       <div className="expense-item__price">{expenseAmount}</div>
+       <h2>{title}</h2>
+       <div className="expense-item__price">{amount}</div>
      </div>
    </div>
   )
