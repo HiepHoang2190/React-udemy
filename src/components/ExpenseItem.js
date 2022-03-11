594e80d54d9934871645202e514ade6c4d9912c5
hieplotus <hieph@lotusoutsourcing.com>
2022-03-11 16:04:47 +0700

    video-35-Adding Basic CSS Styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import './ExpenseItem.css';
 export default function ExpenseItem() {
   return (
-   <div>
-     <div>Date</div>
-     <div>
-       <h2>Title</h2>
-       <div>Amount</div>
+   <div className="expense-item">
+     <div>March 28h 2021</div>
+     <div className="expense-item__description">
+       <h2>Car Insurance</h2>
+       <div className="expense-item__price">$294.67</div>
      </div>
    </div>
   )
